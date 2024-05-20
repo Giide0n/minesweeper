e110fd483943c40a9b0d0b1e6253f8643f3fa00c
gideon <gideon.monterosa@gmail.com>
2024-05-20 23:35:14 +0200

    make it possible to reveal the tiles

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
+const boardState = [];
+
 document.addEventListener('DOMContentLoaded', () => {
   const boardSize = 10;
   const board = document.getElementById('board');
 
   for (let i = 0; i < boardSize; i++) {
+    boardState.push([])
     for (let j = 0; j < boardSize; j++) {
       const tile = document.createElement('div');
       tile.classList.add('tile');
+      tile.row = i;
+      tile.col = j;
+      tile.addEventListener('click', revealTile)
+
+      boardState[i].push('');
 
       board.appendChild(tile);
     }
@@ -14,6 +22,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const tileSize = getTileSize(document.getElementsByClassName('tile').item(0));
   board.style.width = boardSize * tileSize + 'px';
 });
+
+const revealTile = (event) => {
+  const tile = event.currentTarget;
+
+  tile.classList.add('revealed')
+  tile.innerHTML = boardState[tile.row][tile.col];
+}
 
 const getTileSize = (tile) => {
   if(!tile) {
