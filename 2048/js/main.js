97e8ba705e646cd896596442d42ab37c12d8f7cb
gideon <gideon.monterosa@gmail.com>
2024-06-09 15:08:13 +0200

    add a function that spawns numbers in random places

--- 2048/js/main.js
+++ 2048/js/main.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   initializeBoard();
+
+  spawnTile();
+  spawnTile();
 });
 
 const initializeBoard = () => {
@@ -7,7 +10,20 @@ const initializeBoard = () => {
   for (let i = 0; i < 16; i++) {
     const tile = document.createElement('div');
     tile.classList.add('tile');
+    tile.classList.add('empty');
 
     board.appendChild(tile);
   }
 }
+
+const spawnTile = () => {
+  const tiles = document.getElementsByClassName('tile');
+  let r;
+  do {
+    r = Math.floor(Math.random() * 16);
+  } while (!tiles[r].classList.contains('empty'))
+
+  tiles[r].classList.remove('empty');
+  tiles[r].classList.add('number-2')
+}
+
