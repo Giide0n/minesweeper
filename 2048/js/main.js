9ee782cea709f69f74f602ca9ee44d352a64d752
gideon <gideon.monterosa@gmail.com>
2024-06-10 10:07:57 +0200

    work on the swipe logic

--- 2048/js/main.js
+++ 2048/js/main.js
@@ -46,10 +46,16 @@ const spawnTile = () => {
 const swipeRight = () => {
   const tiles = document.getElementsByClassName('tile');
   for (let i = 0; i < 4; i++) {
-    for (let j = 3; j >= 0; j--) {
+    for (let j = 3; j > 0; j--) {
       const idx = 4 * i + j;
-      if(!tiles[idx].classList.contains('empty')) {
-        const numberClass = extractValue(tiles[idx]);
+      const tile = tiles[idx];
+      if(!tile.classList.contains('empty')) {
+        const numberClass = extractValue(tile);
+        tile.classList.remove(numberClass);
+        tile.classList.add('empty')
+
+        tiles[idx + 1].classList.remove('empty')
+        tiles[idx + 1].classList.add(numberClass);
       }
     }
   }
