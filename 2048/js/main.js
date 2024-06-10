3bf9253245ae4f39091ffd832ded4a168517f9f9
gideon <gideon.monterosa@gmail.com>
2024-06-10 08:54:01 +0200

    start the swipe logic

--- 2048/js/main.js
+++ 2048/js/main.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
       case 'ArrowLeft':
         break;
       case 'ArrowRight':
+        swipeRight();
         break;
       default:
         break;
@@ -42,3 +43,23 @@ const spawnTile = () => {
   tiles[r].classList.add('number-2')
 }
 
+const swipeRight = () => {
+  const tiles = document.getElementsByClassName('tile');
+  for (let i = 0; i < 4; i++) {
+    for (let j = 3; j >= 0; j--) {
+      const idx = 4 * i + j;
+      if(!tiles[idx].classList.contains('empty')) {
+        const numberClass = extractValue(tiles[idx]);
+      }
+    }
+  }
+}
+
+const extractValue = (tile) => {
+  for (let i = 0; i < tile.classList.length; i++) {
+    if (tile.classList[i].startsWith('number-')) {
+      return tile.classList[i];
+    }
+  }
+  return null;
+}
